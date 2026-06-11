07729643e1e0b49e16af30e65967a637257808d6
Disha <dishaggarwal03@gmail.com>
2026-06-11 18:47:45 +0530

    Add Task Repository

--- src/main/java/com/disha/taskmanager/TaskRepository.java
+++ src/main/java/com/disha/taskmanager/TaskRepository.java
@@ -1,16 +1,50 @@
 package com.disha.taskmanager;
 import org.springframework.stereotype.Repository;
+
 import java.util.*;
 
 @Repository
 public class TaskRepository {
+
     private List<Task> tasks = new ArrayList<>();
 
-    public Task save(Task task){
+    // Create
+    public Task save(Task task) {
         tasks.add(task);
         return task;
     }
-    public List<Task> findAll(){
+
+    // Read All
+    public List<Task> findAll() {
         return tasks;
+    }
+
+    // Read By Id
+    public Task findById(Long id) {
+        for (Task task : tasks) {
+            if (task.getId().equals(id)) {
+                return task;
+            }
+        }
+        return null;
+    }
+
+    // Update
+    public Task update(Long id, Task updatedTask) {
+        for (Task task : tasks) {
+            if (task.getId().equals(id)) {
+                task.setTitle(updatedTask.getTitle());
+                task.setDescription(updatedTask.getDescription());
+                task.setCompleted(updatedTask.isCompleted());
+
+                return task;
+            }
+        }
+        return null;
+    }
+
+    // Delete
+    public boolean deleteById(Long id) {
+        return tasks.removeIf(task -> task.getId().equals(id));
     }
 }

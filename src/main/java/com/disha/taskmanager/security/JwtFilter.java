b185573ab6ae2702dfcc6f098dd38d6956c9bbd0
Disha <dishaggarwal03@gmail.com>
2026-06-26 16:25:25 +0530

    Add JwtFilter

---
+++ src/main/java/com/disha/taskmanager/security/JwtFilter.java
@@ -0,0 +1,77 @@
+package com.disha.taskmanager.security;
+
+import com.disha.taskmanager.entity.UserEntity;
+import com.disha.taskmanager.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserRepository repository;
+
+    public JwtFilter(JwtService jwtService,
+                     UserRepository repository) {
+        this.jwtService = jwtService;
+        this.repository = repository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+
+        // No token -> continue request
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // Remove "Bearer "
+        String token = authHeader.substring(7);
+
+        // Extract email from JWT
+        String email = jwtService.extractEmail(token);
+
+        // Authenticate only if not already authenticated
+        if (email != null &&
+                SecurityContextHolder.getContext().getAuthentication() == null) {
+
+            UserEntity user = repository.findByEmail(email)
+                    .orElse(null);
+
+            if (user != null && jwtService.isTokenValid(token, user)) {
+
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(
+                                user,
+                                null,
+                                null
+                        );
+
+                authentication.setDetails(
+                        new WebAuthenticationDetailsSource()
+                                .buildDetails(request)
+                );
+
+                SecurityContextHolder.getContext()
+                        .setAuthentication(authentication);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
